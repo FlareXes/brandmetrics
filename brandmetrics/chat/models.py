c9ffb880ccc1b7bb007d9d32602b00616d35e9e5
FlareXes <70066527+FlareXes@users.noreply.github.com>
2025-08-13 19:51:44 +0530

    feat: add employee model

--- brandmetrics/chat/models.py
+++ brandmetrics/chat/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Employee(models.Model):
+    STATUS_CHOICES = [
+        ('Active', 'Active'),
+        ('Inactive', 'Inactive'),
+    ]
+
+    emp_id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="employee_profile")
+    name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    position = models.CharField(max_length=50, blank=True, null=True)
+    city = models.CharField(max_length=50, blank=True, null=True)
+    status = models.CharField(
+        max_length=8, choices=STATUS_CHOICES, default='Active')
+
+    class Meta:
+        db_table = "employees"
+        verbose_name = "Employee"
+        verbose_name_plural = "Employees"
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
